d855599a28b6e34882d6708e7d1fef27d4512d38
RauVR <u201714049@upc.edu.pe>
2023-05-29 20:59:36 -0500

    Initial commit

---
+++ src/main/java/com/crackelets/bigfun/platform/BigFunApplication.java
@@ -0,0 +1,13 @@
+package com.crackelets.bigfun.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigFunApplication.class, args);
+    }
+
+}
