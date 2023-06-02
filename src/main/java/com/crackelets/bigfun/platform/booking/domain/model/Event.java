d04070625b001e7139abd0b54eff134096990fa9
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-02 15:59:52 -0500

    feat: Event class implemented

--- src/main/java/com/crackelets/bigfun/platform/booking/domain/model/Event.java
+++ src/main/java/com/crackelets/bigfun/platform/booking/domain/model/Event.java
@@ -7,7 +7,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,22 +21,59 @@ import lombok.*;
 @AllArgsConstructor
 @With
 @Entity
-@Table(name = "attendee_id")
+@Table(name = "events")
 public class Event extends AuditModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    @Size(min = 10)
     private String name;
+    @Size(max =255)
+    private String address;
 
-    @ManyToOne(fetch = FetchType.EAGER, optional = false)
-    @JoinColumn(name = "skill_id", nullable = false)
+    @NotNull
+    private int capacity;
+
+    @Size(max = 500)
+    private String image;
+
+    @NotNull
+    private Date date;
+    @NotNull
+    private Time hour;
+    @NotNull
+    private int cost;
+
+    @Size(max = 40)
+    @NotNull
+    private String district;
+
     @JsonIgnore
-    private Attendee attendee;
+    @ManyToMany(mappedBy = "events")
+    private Set<Attendee> attendees = new HashSet<>();
+
+
+
+
 
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "organizer_id", nullable = false)
     @JsonIgnore
     private Organizer organizer;
+
+    public Event withAttendee(Attendee attendee) {
+
+        if (attendees == null){
+            attendees = new HashSet<>();
+        }
+
+        attendees.add(attendee);
+        return this;
+
+    }
 }
