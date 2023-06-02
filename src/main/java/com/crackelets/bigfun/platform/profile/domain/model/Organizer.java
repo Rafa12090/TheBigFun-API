eae0b2170eec54a1e3796250293177579ffdff8f
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-01 23:53:25 -0500

    feat: Organizer class implemented. Relationship between Organizer and Event

--- src/main/java/com/crackelets/bigfun/platform/profile/domain/model/Organizer.java
+++ src/main/java/com/crackelets/bigfun/platform/profile/domain/model/Organizer.java
@@ -1,7 +1,67 @@
 package com.crackelets.bigfun.platform.profile.domain.model;
 
+import com.crackelets.bigfun.platform.booking.domain.model.Event;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "organizers")
+
 public class Organizer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Autogenerate value
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String userName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
+    private String email;
+
+
+    //Relationships
+
+    //private EventList eventList;
+    @OneToMany(cascade = CascadeType.ALL,fetch=FetchType.EAGER,mappedBy="organizer")
+    private Set<Event> events = new HashSet<>();
+
+    //business rule
+    public Organizer addEvent(String eventName) {
+
+        if (events == null) {
+            events = new HashSet<>();
+        }
+
+        //add event to organizer
+        events.add(new Event()
+                .withName(eventName)
+                .withOrganizer(this));
+        return this;
+    }
+
+    //private PaymentList paymentList;  ->queda pendiente crear instancia en pay
 
 
 }
