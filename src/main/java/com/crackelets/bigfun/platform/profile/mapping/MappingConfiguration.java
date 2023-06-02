426c870e589311c4f8eeed5803f829ee0634044c
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-02 14:30:57 -0500

    fix: update MappingConfiguration in profile

--- src/main/java/com/crackelets/bigfun/platform/profile/mapping/MappingConfiguration.java
+++ src/main/java/com/crackelets/bigfun/platform/profile/mapping/MappingConfiguration.java
@@ -3,11 +3,12 @@ package com.crackelets.bigfun.platform.profile.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("learningMappingConfiguration") //sirve para diferenciarse de la clase que esta en Learning
+@Configuration("profileMappingConfiguration")
 
 public class MappingConfiguration {
     @Bean
     public OrganizerMapper organizerMapper(){
+
         return new OrganizerMapper();
     }
 }
