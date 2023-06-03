2ab347825d54f99d7af339f6922f1ffe6c45d55c
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-02 20:10:14 -0500

    Feat: Event resource implemented

---
+++ src/main/java/com/crackelets/bigfun/platform/booking/resource/EventResource.java
@@ -0,0 +1,31 @@
+package com.crackelets.bigfun.platform.booking.resource;
+
+import lombok.*;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EventResource {
+
+    private Long id;
+    private String name;
+    private String address;
+    private int capacity;
+    private String image;
+    private Date date;
+    private Time hour;
+    private int cost;
+    private String district;
+
+
+
+
+
+
+}
