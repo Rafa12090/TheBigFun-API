774771ed18990d3104d91facf17f604d19ea27fa
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-02 20:11:51 -0500

    Feat: Event Controller created

---
+++ src/main/java/com/crackelets/bigfun/platform/booking/api/rest/EventsController.java
@@ -0,0 +1,12 @@
+package com.crackelets.bigfun.platform.booking.api.rest;
+
+
+import com.crackelets.bigfun.platform.booking.domain.service.EventService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/api/v1/events", produces= "application/json")
+public class EventsController {
+
+}
