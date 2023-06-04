ed5932211d4733f28d7387a329283c3b5dde6773
RauVR <u201714049@upc.edu.pe>
2023-06-03 20:35:26 -0500

    refactor : Class AttendeeMapper refactorized

--- src/main/java/com/crackelets/bigfun/platform/profile/mapping/AttendeeMapper.java
+++ src/main/java/com/crackelets/bigfun/platform/profile/mapping/AttendeeMapper.java
@@ -5,6 +5,7 @@ import com.crackelets.bigfun.platform.profile.resource.AttendeeResource;
 import com.crackelets.bigfun.platform.profile.resource.CreateAttendeeResource;
 import com.crackelets.bigfun.platform.profile.resource.UpdateAttendeeResource;
 import com.crackelets.bigfun.platform.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,7 @@ import java.util.List;
 
 public class AttendeeMapper implements Serializable {
 
+    @Autowired
     private EnhancedModelMapper mapper;
 
     public AttendeeResource toResource(Attendee model){ return mapper.map(model, AttendeeResource.class); }
