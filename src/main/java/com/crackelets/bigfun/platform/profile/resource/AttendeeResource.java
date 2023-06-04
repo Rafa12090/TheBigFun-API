df0f555b8b952af26b4090e4b1e70e75d46adc28
RauVR <u201714049@upc.edu.pe>
2023-06-03 19:13:24 -0500

    feat : AttendeeResource Class Implemented

---
+++ src/main/java/com/crackelets/bigfun/platform/profile/resource/AttendeeResource.java
@@ -0,0 +1,20 @@
+package com.crackelets.bigfun.platform.profile.resource;
+
+import com.crackelets.bigfun.platform.booking.domain.model.Event;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttendeeResource {
+
+    private Long id;
+    private String userName;
+    private String name;
+    private String email;
+    //private Set<Event> events;
+}
