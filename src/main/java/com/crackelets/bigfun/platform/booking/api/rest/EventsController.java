bf0a93c44235516793177e462f15b8389753036c
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-04 20:05:48 -0500

    feat: event controller implemented

--- src/main/java/com/crackelets/bigfun/platform/booking/api/rest/EventsController.java
+++ src/main/java/com/crackelets/bigfun/platform/booking/api/rest/EventsController.java
@@ -2,11 +2,55 @@ package com.crackelets.bigfun.platform.booking.api.rest;
 
 
 import com.crackelets.bigfun.platform.booking.domain.service.EventService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.crackelets.bigfun.platform.booking.mapping.EventMapper;
+import com.crackelets.bigfun.platform.booking.resource.CreateEventResource;
+import com.crackelets.bigfun.platform.booking.resource.EventResource;
+import com.crackelets.bigfun.platform.booking.resource.UpdateEventResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value= "/api/v1/events", produces= "application/json")
+@RequestMapping(value= "/api/v1/events")
 public class EventsController {
+
+    private  final EventService eventService;
+
+    private final EventMapper mapper;
+
+
+    public EventsController(EventService eventService, EventMapper mapper) {
+        this.eventService = eventService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public Page<EventResource> getAllEvents(Pageable pageable){
+        return mapper.modelListPage(eventService.getAll(), pageable);
+    }
+
+    @GetMapping ("{eventId}")
+    public EventResource getEventById(@PathVariable Long eventId){
+        return mapper.toResource(eventService.getById(eventId));
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResource> createEvent(CreateEventResource resource){
+        return new ResponseEntity<>(mapper.toResource(eventService.create(mapper.toModel(resource))), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{eventId}")
+    public EventResource updateEvent(@PathVariable Long eventId,
+                                     @RequestBody UpdateEventResource resource){
+        return mapper.toResource(eventService.update(eventId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{eventId}")
+    public ResponseEntity<?> deleteEvent(@PathVariable Long eventId){ return eventService.delete(eventId);}
+
+
+
 
 }
