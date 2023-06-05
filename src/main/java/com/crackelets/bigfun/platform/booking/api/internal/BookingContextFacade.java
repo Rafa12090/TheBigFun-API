72fe51456978ce3b4f8cec7cba03f431db857efb
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-04 19:59:45 -0500

    Feat: Booking Context Facade implemented

--- src/main/java/com/crackelets/bigfun/platform/booking/api/internal/BookingContextFacade.java
+++ src/main/java/com/crackelets/bigfun/platform/booking/api/internal/BookingContextFacade.java
@@ -1,5 +1,11 @@
 package com.crackelets.bigfun.platform.booking.api.internal;
 
+import com.crackelets.bigfun.platform.booking.domain.model.Event;
+
+import java.util.List;
+
 public interface BookingContextFacade {
-    
+
+   List<Event> getAllEvents();
+
 }
