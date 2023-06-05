9540bff45c8d6cff74ccabd264c0159b1006f56b
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-04 20:03:55 -0500

    feat: - Add validation annotations to the name, address, and district fields - Update size constraints for the name and address fields - Rename the cost field to eventCost for clarity - Improve variable names and formatting for better readability

--- src/main/java/com/crackelets/bigfun/platform/booking/resource/CreateEventResource.java
+++ src/main/java/com/crackelets/bigfun/platform/booking/resource/CreateEventResource.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.sql.Time;
+import java.util.Date;
+
 @Getter
 @Setter
 @With
@@ -17,5 +20,30 @@ public class CreateEventResource {
     @NotNull
     @NotBlank
     @Size(max= 50)
+    @Size(min = 10)
     private String name;
+
+
+    @Size(max =240)
+    @Size(min = 100)
+    private String address;
+
+
+    @NotNull
+    private int capacity;
+
+
+    @NotNull
+    private Date date;
+    @NotNull
+    private Time hour;
+    @NotNull
+    private int cost;
+
+    @Size(max = 50)
+    @Size(min = 20)
+    @NotNull
+    private String district;
+
+
 }
