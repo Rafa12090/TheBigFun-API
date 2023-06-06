01431724558756d7878890f80dee59fef04739ee
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-06 09:54:52 -0500

    refactor : Event class updated

--- src/main/java/com/crackelets/bigfun/platform/booking/domain/model/Event.java
+++ src/main/java/com/crackelets/bigfun/platform/booking/domain/model/Event.java
@@ -68,5 +68,35 @@ public class Event extends AuditModel {
         this.attendees.add(new EventAttendee(this, attendeeId));
     }
 
+    
+    /*package com.crackelets.bigfun.platform.booking.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+
+@NoArgsConstructor
+@With
+@Entity
+@Table(name="event_attendees")
+public class EventAttendee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Event event;
+    private Long attendeeId;
+
+
+    public EventAttendee(Event event, Long attendeeId) {
+        this.event = event;
+        this.attendeeId = attendeeId;
+    }
+}*/
+    
 
 }
