82cd8c5c7542590f35a682be4d278a912d6bdf7a
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-21 17:07:54 -0500

    feat: added jwt handler.

--- src/main/java/com/crackelets/bigfun/platform/security/middleware/JwtHandler.java
+++ src/main/java/com/crackelets/bigfun/platform/security/middleware/JwtHandler.java
@@ -1,6 +1,10 @@
 package com.crackelets.bigfun.platform.security.middleware;
 
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtHandler {
@@ -23,8 +30,60 @@ public class JwtHandler {
     @Value("$")
 
     public String generateToken(Authentication authentication){
-        //String subject = ()
-        return "";
+        String subject = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
+        Date issueAt = new Date();
+        Date expiration = DateUtils.addDays(issueAt, expirationDays);
+        Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(issueAt)
+                .setExpiration(expiration)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFrom(String token){
+        try{
+            return Jwts.parserBuilder()
+                    .setSigningKey(secret.getBytes())
+                    .build()
+                    .parseClaimsJwt(token)
+                    .getBody()
+                    .getSubject();
+        } catch (SignatureException e){
+            logger.error("Invalid JSON Web Token Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e){
+            logger.error("Invalid JSON Web Token: {}", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JSON Web Token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JSON Web Token Format is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error("JSON Web Token claims string is empty: {}", e.getMessage());
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(secret.getBytes())
+                    .build()
+                    .parseClaimsJwt(token);
+            return true;
+        } catch (SignatureException e){
+            logger.error("Invalid JSON Web Token Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e){
+            logger.error("Invalid JSON Web token: {}", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JSON Web Token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JSON Web Token Format is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error("JSON Web Token claims string is empty: {}", e.getMessage());
+        }
+        return false;
     }
 
 
